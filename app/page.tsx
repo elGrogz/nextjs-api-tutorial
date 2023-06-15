8a0932e08976205235b60cff5feed654ebb3da46
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-06-15 20:52:10 +0100

    added paragraph component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Paragraph from "./components/Paragraph";
 
 export default function Home() {
   return (
     <main className="bg-red-500 flex min-h-screen flex-col items-center justify-between p-24">
       hiya
+      <Paragraph size="sm">HELLLOO</Paragraph>
     </main>
   );
 }
