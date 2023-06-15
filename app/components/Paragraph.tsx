a4977147470c1091deb5276f89535d47b45c5a23
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-06-15 21:18:02 +0100

    takin' notes

--- app/components/Paragraph.tsx
+++ app/components/Paragraph.tsx
@@ -2,26 +2,33 @@ import { HTMLAttributes, forwardRef } from "react";
 import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "../lib/utils";
 
+// This makes the paragraph component reusable using the class variance authority libary (basically defines which styles to apply to the component depending on the props passed in)
 const paragraphVariants = cva(
-  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center", // Tailwind styling that applies for everything in the component that implements paragraph variants
   {
     variants: {
+      // the properties in variants are available as props for the parent components rendering this component
+      // different variants we want for the paragraph
       size: {
-        default: "text-base sm:text-lg", //
-        sm: "text-sm, sm:text-base",
+        default: "text-base sm:text-lg", // text-base determines a tailwind font size (this is default)
+        sm: "text-sm sm:text-base",
+        blah: "text-sm bloo",
       },
     },
     defaultVariants: {
+      // this will apply the default variant as the default
       size: "default",
     },
   }
 );
 
+// define the interface for the props that will be passed in
 interface ParagraphProps
-  extends HTMLAttributes<HTMLParagraphElement>,
-    VariantProps<typeof paragraphVariants> {}
+  extends HTMLAttributes<HTMLParagraphElement>, // HTMLAttributes is a react type that takes a generic (like a function that takes types instead of parameters. It takes an HTMLParagraph type which means any component that uses the Paragraph component can see the props that are available to an HTML paragraph
+    VariantProps<typeof paragraphVariants> {} //
 
 const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+  // everything in the props (first) argument of the function component will be of type HTMLParagraphElement and Paragraph Props
   ({ className, size, children, ...props }, ref) => {
     return (
       <p
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Paragraph from "./components/Paragraph";
 
 export default function Home() {
