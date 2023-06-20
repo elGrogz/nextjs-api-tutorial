da9755326f72711867d1d9fe8ff5b1a6ab6c0d16
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-06-20 22:42:02 +0100

    starting tabs

--- app/documentation/layout.tsx
+++ app/documentation/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
 
+// automatically renders thanks for next paths
+
 export default function Layout({ children }: { children: ReactNode }) {
-  return <section className="pt-10">{children}</section>;
+  return <section className="pt-20">{children}</section>;
 }
--- app/documentation/page.tsx
+++ app/documentation/page.tsx
@@ -2,7 +2,8 @@ import { FC } from "react";
 
 import type { Metadata } from "next";
 import LargeHeading from "@/ui/LargeHeading";
-import Layout from "./layout";
+import Paragraph from "@/ui/Paragraph";
+import DocumentationTabs from "@/components/DocumentationTabs";
 
 export const metadata: Metadata = {
   title: "Similarity API | Documentation",
@@ -13,13 +14,14 @@ interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
   return (
-    <Layout>
-      <div className="container max-w-7xl mx-auto mt-12">
-        <div className="flex flex-col items-center gap-6">
-          <LargeHeading>Documentation</LargeHeading>
-        </div>
+    <div className="container max-w-7xl mx-auto mt-12">
+      <div className="flex flex-col items-center gap-6">
+        <LargeHeading>Documentation</LargeHeading>
+        <Paragraph>api/v1/similarity</Paragraph>
+
+        <DocumentationTabs />
       </div>
-    </Layout>
+    </div>
   );
 };
 
