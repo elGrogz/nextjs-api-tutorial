8034012f6c08da4bc0aad0ede343020ea5018e76
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-06-21 22:45:49 +0100

    starting tabs

--- app/components/DocumentationTabs.tsx
+++ app/components/DocumentationTabs.tsx
@@ -1,7 +1,18 @@
 import { FC } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/Tabs";
+import SimpleBar from "simplebar-react";
 
 const DocumentationTabs: FC = () => {
-  return <div>DocumentationTabs</div>;
+  return (
+    <Tabs defaultValue="nodejs" className="max-w-2xl w-full">
+      <TabsList>
+        <TabsTrigger value="nodejs">NodeJS</TabsTrigger>
+        <TabsTrigger value="python">Python</TabsTrigger>
+      </TabsList>
+      <TabsContent value="nodejs">{/* <SimpleBar></SimpleBar> */}</TabsContent>
+      <TabsContent value="python"></TabsContent>
+    </Tabs>
+  );
 };
 
 export default DocumentationTabs;
