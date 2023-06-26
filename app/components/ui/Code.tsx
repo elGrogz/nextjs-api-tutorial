a9926901486b3eab568c0b352bda52c675fcfa9b
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-06-26 22:55:18 +0200

    comments

--- app/components/ui/Code.tsx
+++ app/components/ui/Code.tsx
@@ -4,6 +4,7 @@ import { FC, useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { Highlight, themes } from "prism-react-renderer";
 
+// set the props passed into the component
 interface CodeProps {
   code: string;
   show: boolean;
@@ -19,26 +20,29 @@ const Code: FC<CodeProps> = ({
   animationDelay,
   animated,
 }) => {
-  const { theme: applicationTheme } = useTheme();
-  const [text, setText] = useState(animated ? "" : code);
+  const { theme: applicationTheme } = useTheme(); // use the nextjs theme
+  const [text, setText] = useState(animated ? "" : code); // if the component is to be animated then start empty so the component can fill up, if not then just show everything
 
   useEffect(() => {
     if (show && animated) {
-      let i = 0;
+      // only run the use effect if we want to show the code and if we want to animate it
+      let i = 0; // start the counter at 0
       setTimeout(() => {
+        // setTimeout starts something after the value in the second argument - either from the parent component as a prop, or a set value of 150
         const intervalId = setInterval(() => {
-          setText(code.slice(0, i));
-          i++;
+          // setInterval does something per x seconds from the second argyment (15 ms)
+          setText(code.slice(0, i)); // replaces the current code with all characters between the first character of the code and the character at position i
+          i++; //incremenet the count
 
           if (i > code.length) {
-            clearInterval(intervalId);
+            clearInterval(intervalId); // stop the intervals (code expanding) when we reach the end of the characters
           }
 
-          return () => clearInterval(intervalId);
+          return () => clearInterval(intervalId); // stop the intervals if the component unmounts to prevent memory leaks
         }, 15);
       }, animationDelay || 150);
     }
-  }, [code, show, animated, animationDelay]);
+  }, [code, show, animated, animationDelay]); // rerun the use effect if the code, show, animated or animationDelay props change (only going to be code in this case)
 
   // number of lines
 
